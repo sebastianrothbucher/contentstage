7c1d40a5889c85f24fcce890b9bd6db4cc783f82
Sebastian Rothbucher <sebastianrothbucher@googlemail.com>
2018-11-25 12:42:32 +0100

    Add Readme and extend extract for stashing

---
+++ README.md
@@ -0,0 +1,23 @@
+# contentstage
+
+Export (*extract*) page and category contents of a magento shop and re-import (*apply*) it to another. Effectively allows for a staging area to be used to develop content. 
+
+## What *extract* does
+
+* extract pages by identifier and store code (*admin* might just be *all* (id 0))
+* (categories are to come)
+* save to ```shop_content.json``` (in folder contents) and check in to git
+
+## What *apply* does
+
+* (it needs shop_content.json to be fully checked in)
+* *extract* once more, and **stash** a potentially changed ```shop_content.json``` (so we have a backup)
+* take ```shop_content.json``` and update pages with an update timestamp smaller (or equal) to ours
+* (set the update timestamp to ours afterwards)
+* (so far) show an error when update timestamp bigger (pot. conflict)
+* create pages that don't exist yet
+
+## potential extensions
+
+* show diff on detected conflict
+* contents of products, blocks, etc.
--- extract.js
+++ extract.js
@@ -5,11 +5,12 @@ const mysql = require('mysql');
 console.log("Exporting Page & Category content");
 const connection = mysql.createConnection(require('./connection'));
 let pageres = new Promise((resolve, rejectt) => connection.query(
-        "select p.identifier as p_identifier, p.title as p_title, p.page_layout as p_page_layout, " + 
-        "p.content_heading as p_content_heading, p.content as p_content, p.update_time as p_update_time, " + 
-        "p.is_active as p_is_active, s.code as s_code " + 
+        "select p.identifier as p_identifier, s.code as s_code, p.title as p_title, " + 
+        "p.page_layout as p_page_layout, p.content_heading as p_content_heading, " + 
+        "p.content as p_content, p.update_time as p_update_time, p.is_active as p_is_active " + 
         "from cms_page p join cms_page_store ps on p.page_id=ps.page_id " + 
-        "join store s on ps.store_id=s.store_id", (err, res) => { // seems we don't need URLs so far
+        "join store s on ps.store_id=s.store_id " + 
+        "order by p.identifier, s.code", (err, res) => { // seems we don't need URLs so far
     if (err) {
         reject(err);
     } else {
@@ -30,7 +31,9 @@ Promise.all([
     // TODO: save 2 file in ./contents + invoke git (if -d .git)
     const serfile = './contents/shop_content.json';
     fs.writeFileSync(serfile, JSON.stringify(serialized, null, 4), 'utf-8');
-    if (fs.existsSync('./.git') && fs.lstatSync('./.git').isDirectory()) {
+    if (process.argv.filter(a => a == '--nogit').length == 0 && 
+            fs.existsSync('./.git') && 
+            fs.lstatSync('./.git').isDirectory()) {
         childproc.execSync('git add ' + serfile);
         childproc.execSync('git commit -m "Update shop content"');
     }
